f8f5298533101aaa6a865821f40a6f1295c8fc2b
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2018-07-21 21:21:04 -0300

    Update conectados.sh

--- conectados.sh
+++ conectados.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Descobre o nro de ip da rede local:
 ips=$(hostname -I)
 
+# Fatia apenas a parte necessária para varrer...:
 sliceIp=$(echo $ips | cut -d'.' -f 1,2,3)
- 
-nmap -sP $sliceIp.* | grep report | sed 's/Nmap scan report for//'
 
+# Procura quem está conectado na sua rede:   
+nmap -sP $sliceIp.* | grep report | sed 's/Nmap scan report for//'
 
 # Autor: Helio Giroto
